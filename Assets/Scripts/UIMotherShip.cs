098232616c38333e5c2b0cb8b8383158adb9966d
Eduardo Cobuci <educobuci@gmail.com>
2015-11-05 00:40:24 -0200

    Add energy

---
+++ Assets/Scripts/UIMotherShip.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIMotherShip : MonoBehaviour {
+
+    private MotherShip motherShip;
+    public int collected;
+    public int needed;
+
+	// Use this for initialization
+	void Awake () {
+        motherShip = GameObject.Find("MotherShip").GetComponent<MotherShip>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        collected = motherShip.collectedEnergy;
+        needed = motherShip.neededEnergy;
+
+        GetComponent<Text>().text = string.Format("{0} / {1}", collected, needed);
+	}
+}
---
+++ Assets/Scripts/UITextHero.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class UITextHero : MonoBehaviour {
+
+    private PlayerInventory playerInvetory;
+    public int playerEnergy;
+
+	// Use this for initialization
+	void Awake () {
+        playerInvetory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerEnergy = playerInvetory.collectedEnergy;
+        GetComponent<Text>().text = playerInvetory.collectedEnergy.ToString();
+	}
+}
